071968a487e556136bb4a82e4cabfdf5fc39e5c4
GaganLal <lalgagan@outlook.com>
2019-01-23 23:14:28 +0000

    form validation (constraints) assignment done

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,54 +1,54 @@
 $(document).ready(function() {
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
     },
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  }
 
-function createTweetElement(tweet) {
+  function createTweetElement(tweet) {
 
 
 
     return `<article>
-          <header> <img class ="img" src="https://c.ndtvimg.com/2018-10/15p50r1g_cristiano-ronaldo-afp-10-18_625x300_03_October_18.jpg?output-quality=70&output-format=webp">
-           <div class="name"> ${tweet.user.name} </div>   <div class ="short">  cr7 </div>
+    <header> <img class ="img" src="https://c.ndtvimg.com/2018-10/15p50r1g_cristiano-ronaldo-afp-10-18_625x300_03_October_18.jpg?output-quality=70&output-format=webp">
+    <div class="name"> ${tweet.user.name} </div>   <div class ="short">  cr7 </div>
 
-          </header>
-          <div class="lorem">${tweet.content.text}</div>
-
-
-        <footer> 10 Days ago
-          <div class="emoji">
-          <a href="#"> <i class="fas fa-flag">  </i> </a>
-
-          <a href="#"> <i class="fas fa-retweet"> </i> </a>
-
-          <a href="#"> <i class="fas fa-heart"> </i> </a>
+    </header>
+    <div class="lorem">${tweet.content.text}</div>
 
 
+    <footer> 10 Days ago
+    <div class="emoji">
+    <a href="#"> <i class="fas fa-flag">  </i> </a>
 
-        </footer>
+    <a href="#"> <i class="fas fa-retweet"> </i> </a>
+
+    <a href="#"> <i class="fas fa-heart"> </i> </a>
 
 
 
-      </article>`
+    </footer>
 
-}
 
-createTweetElement(tweetData);
 
-const data = [
+    </article>`
+
+  }
+
+  createTweetElement(tweetData);
+
+  const data = [
   {
     "user": {
       "name": "Newton",
@@ -73,75 +73,87 @@ const data = [
         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
       },
       "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      "content": {
+        "text": "Je pense , donc je suis"
       },
-      "handle": "@johann49"
+      "created_at": 1461113959088
     },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+    ];
 
 
 
 
-function renderTweets(data) {
+    function renderTweets(data) {
 
-data.forEach(function (tweet) {
-  $(".tweets-section").append(createTweetElement(tweet))
+      data.forEach(function (tweet) {
+        $(".tweets-section").append(createTweetElement(tweet))
 
-})
-}
+      })
+    }
 
-renderTweets(data)
+    renderTweets(data)
 
-$( "form" ).on("submit", function( event ) {
+    $( "form" ).on("submit", function( event ) {
   event.preventDefault();                                 // function to prevent reload. deactivating the FORM SUBMIT
 
-var form = $(this)
-console.log(form.serialize())
-$.ajax({
-        url: '/tweets',
+  var form = $(this)
+
+  let $textBox = $(".new-tweet textarea")
+  let tweetValue = $textBox.val()
+  let tweetLength = tweetValue.length
+
+  if(tweetLength > 140 ) {
+    alert("Tweet exceeded Limit")                         // here adding a constraint on the text box,
+  } else if (!tweetValue) {                                     //tweet has to meet these requirements to submit
+    alert("There is no tweet")
+  } else {
+
+  console.log(form.serialize())
+  $.ajax({
+    url: '/tweets',
         type: 'POST',                                       // making a ajax request similar to action="/tweets"
         data:  form.serialize(),                                      //method = POST
         success: function (response) {                  // so sending tweets to server
-            alert(response);
+          alert(response);
         },
         error: function () {
-            alert("error");
+          alert("error");
         }
-    });
+      });
+};
 
 
 });
 
-function loadTweets() {
-  $.ajax("/tweets", {
-    method: "GET",
+    function loadTweets() {
+      $.ajax("/tweets", {
+        method: "GET",
     dataType: 'json'                                          //fetching data from the server
   })
-  .then(function (profile) {
-    renderTweets(profile)
+      .then(function (profile) {
+        renderTweets(profile)
+
+      })
+    }
+
+    loadTweets()
 
   })
-}
-
-loadTweets()
-
-})
 
 
 
